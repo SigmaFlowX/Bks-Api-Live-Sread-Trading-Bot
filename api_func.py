b1af9b0c1fa59037a5d5a98f35259eda1986eae8
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-01-04 15:31:23 +0800

    Added get_current_price function

--- api_func.py
+++ api_func.py
@@ -1,10 +1,10 @@
 import requests
+from datetime import datetime, timedelta, timezone
 
 def get_token_from_txt_file():
     file = open("token.txt")
     token = file.read()
     return token
-
 
 def authorize(refresh_token):
     url = "https://be.broker.ru/trade-api-keycloak/realms/tradeapi/protocol/openid-connect/token"
@@ -28,8 +28,44 @@ def authorize(refresh_token):
         return token
     else:
         print("Error while trying  to authorize:", response.status_code, response.text)
-        return 0
+        return None
+
+def get_current_price(ticker, token,class_code = "TQBR"):
+    url = "https://be.broker.ru/trade-api-market-data-connector/api/v1/candles-chart"
+
+    end_date = datetime.now(timezone.utc)
+    start_date = end_date - timedelta(days=40)
+    start_date_str = start_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+    end_date_str = end_date.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+    headers = {
+        "Accept": "application/json",
+        "Authorization": f"Bearer {token}"
+    }
+
+    payload = {
+        "classCode": class_code,
+        "ticker": ticker,
+        "startDate": start_date_str,
+        "endDate": end_date_str,
+        "timeFrame": "MN"
+    }
+
+    response = requests.get(url, headers=headers, params=payload)
+
+    if response.status_code == 200:
+        data = response.json()
+        candles = data.get("bars", [])
+        if candles:
+            return candles[0]['close']
+        else:
+            print("No candles obtained")
+            return None
+    else:
+        print("Failed to get current price:", response.status_code, response.text)
+        return None
 
 
 if __name__ == "__main__":
     access_token = authorize(get_token_from_txt_file())
+    print(get_current_price("SBER", access_token))
