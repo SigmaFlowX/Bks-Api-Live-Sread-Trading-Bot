fe1ec84e1fe93e42574859b98f2f5172e67c0cbd
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-01-04 16:12:58 +0800

    Added placing orders and gettins order status fucntions

--- api_func.py
+++ api_func.py
@@ -1,5 +1,8 @@
+from http.client import responses
+
 import requests
 from datetime import datetime, timedelta, timezone
+import uuid
 
 def get_token_from_txt_file():
     file = open("token.txt")
@@ -30,7 +33,7 @@ def authorize(refresh_token):
         print("Error while trying  to authorize:", response.status_code, response.text)
         return None
 
-def get_current_price(ticker, token,class_code = "TQBR"):
+def get_current_price(token, ticker, class_code = "TQBR"):
     url = "https://be.broker.ru/trade-api-market-data-connector/api/v1/candles-chart"
 
     end_date = datetime.now(timezone.utc)
@@ -65,7 +68,66 @@ def get_current_price(ticker, token,class_code = "TQBR"):
         print("Failed to get current price:", response.status_code, response.text)
         return None
 
+def place_order(token, ticker, quantity, direction,  order_type, class_code="TQBR", price=None):  # 1 - buy, 2 - sell, 1 - market, 2 - limit
+
+    url = "https://be.broker.ru/trade-api-bff-operations/api/v1/orders"
+
+    client_order_id = str(uuid.uuid4())
+
+    headers = {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+      "Authorization": f"Bearer {token}"
+    }
+
+    payload = {
+        "clientOrderId": client_order_id,
+        "side": str(direction),
+        "orderType": str(order_type),
+        "orderQuantity": quantity,
+        "ticker": ticker,
+        "classCode": class_code,
+    }
+    if order_type == 2:
+        payload["price"] = float(price)
+
+    response = requests.post(url, headers=headers, json=payload)
+
+    if response.status_code == 200:
+        print("Order placed")
+        return client_order_id
+    else:
+        print("Failed to place the order:", response.status_code, response.text)
+        return None
+
+def get_order_status(token, id):
+    url = f"https://be.broker.ru/trade-api-bff-operations/api/v1/orders/{id}"
+
+    payload = {
+        "originalClientOrderId": id
+    }
+
+    headers = {
+        'Accept': 'application/json',
+        'Authorization': f'Bearer {token}'
+    }
+
+    response = requests.request("GET", url, headers=headers, data=payload)
+
+    return response.json()['data']['orderStatus']
+
+# Состояние заявки:
+# 0 — Новая
+# 1 — Частично исполнена
+# 2 — Полностью исполнена
+# 4 — Отменена
+# 5 — Заменена
+# 6 — Отменяется (в процессе отмены)
+# 8 — Отклонена
+# 9 — Заменяется (например, если вы изменяли заявку)
+# 10 — Ожидает подтверждения новой заявки
 
 if __name__ == "__main__":
     access_token = authorize(get_token_from_txt_file())
-    print(get_current_price("SBER", access_token))
+    order = place_order(access_token, "SBER", 1, 1, 2, price = 100)
+    print(get_order_status(access_token, order))
